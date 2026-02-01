9dbc1ff3b33029c5c061707dd5c02a1b869e52b2
Dylan Styles <dylanstyles34@gmail.com>
2026-02-01 16:00:28 +0000

    Update label_grooming.py
    Added docstring for grooming labelling script

--- scripts/label_grooming.py
+++ scripts/label_grooming.py
@@ -1,1 +1,10 @@
 """
+label_grooming.py - PASYDA grooming labelling script
+
+Labelling script PASYDA datasets.
+Labels each conversation as:
+- yes > grooming detected
+- no > grooming not detected
+
+Uses behvaioural metadata to generate labels.
+"""
